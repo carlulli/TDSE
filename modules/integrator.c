e95946fc05e893ea84e91f448277b4e5aecb892d
darlhet <76957966+darlhet@users.noreply.github.com>
2021-02-15 12:40:47 +0100

    working integrators

--- modules/integrator.c
+++ modules/integrator.c
@@ -1,4 +1,6 @@
 #include <complex.h>
+
+#include "conjugategradient.h"
 #include "linearalgebra.h"
 #include "hamiltonian.h"
 #include "geometry.h"
@@ -6,16 +8,15 @@
 /* time step of the integration method */
 static double time_step;
 
-/* takes array and integration step */
+/* takes array and integration step , modifies the array input*/
 void euler_method(double complex *in, double tau) {
-
+  /* uses the geometry.h library to obtain the N number of lattice points */
   int N = get_N();
   double complex Hpsi[N];
-
   H(in,Hpsi);
 
   for(int i = 0; i < N; i++) {
-    in[i] -= tau*Hpsi[i]*I;
+    in[i] = in[i] - tau*Hpsi[i]*I;
   }
 }
 
@@ -24,28 +25,31 @@ void euler_method(double complex *in, double tau) {
 /* function to use with conjugate_gradient() , applies the operator (1 +t*t/4*H*H) psi and gives back out */
 void operator(double complex *in, double complex *out) {
   int N = get_N();
-  double complex Hpsi[N];
-  H(in,Hpsi);
-  H(Hpsi,Hpsi);
+  //double complex Hpsi[N];
+  H(in,in);
+  H(in,in);
   for(int i = 0; i < N; i ++) {
-    out[i] = 1 + 1/4*time_step*time_step*Hpsi[i];
+    out[i] = 1 + 1/4*time_step*time_step*in[i];
   }
 }
 
 /* uses conjugategradient to calculate inverse of the operator it needs */
 void UCN_method(double complex *in,double tau) {
-  time_step = tau;
+  //time_step = tau;
   int N = get_N();
   double complex Hnu[N],HHnu[N], nu[N];
+  void (*op_ptr)(double complex *, double complex *);
+  op_ptr = &operator;
 
   for(int i = 0; i < N; i++) {
 
   }
 
-  conjugate_gradient(in,nu,&operator);
+  conj_grad(in,nu,(*op_ptr));
 
   H(nu,Hnu);
   H(Hnu,HHnu);
+  
   for(int i = 0; i < N; i++) {
     in[i] = nu[i] - tau*Hnu[i]*I -1/4*tau*tau*HHnu[i];
   }
---
+++ test/integrator_test.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "geometry.h"
+#include "linearalgebra.h"
+#include "hamiltonian.h"
+#include "wavefunction.h"
+#include "integrator.h"
+#include "assert.h"
+#include "conjugategradient.h"
+
+/* in this routine the correct functioning of the various integration methods implemented */
+
+int main  (int argc, char *argv[]) {
+
+  return 0;
+}

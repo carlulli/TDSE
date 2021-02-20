7b48edc2774a476188349b97fd0979d77330781c
Adronis <astistroth@gmail.com>
2021-02-21 00:30:34 +0100

    test_gaussian_wf compiles with make

--- test/test_gaussian_wf.c
+++ test/test_gaussian_wf.c
@@ -8,10 +8,7 @@
 
 #include "geometry.h"
 #include "linearalgebra.h"
-//#include "hamiltonian.h"
 #include "wavefunction.h"
-//#include "integrator.h"
-
 
 #define _FILE_NAME_ "test/test_gaussion_wf.c"
 
@@ -24,21 +21,18 @@
 int main(int argc, char *argv[]) {
 
   /* Here the parameters are passed in the set_params function, which checks the validity */
+  /* some params are not used and not called (get function) */
   set_params(argc, (char**) argv);
-  /* mass is always hardcoded */
-  double mass = 2.3512;
-  N = get_N();
-  double time = get_time();
-  double nsteps = get_nsteps();
-  double tau = time/nsteps;
-  int integrator_choice = get_integ_choice();
-  int pot_choice = get_pot_choice();
+
+  int N = get_N();
   double mu = atof(argv[6]);
   double dx = atof(argv[7]);
   double dp = atof(argv[8]);
-  set_kinetic_params(mass);
-  set_potential(pot_choice);
-  print_hamiltonian_info();
+
+  /* wavefunction */
+  double complex *psi;
+  psi = (double complex*) malloc(sizeof(double complex)*N);
+
   set_gaussian_wavefunction(psi,mu,dx,dp,N);
 
   for(int i = 0; i < N; i++) {

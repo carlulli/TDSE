41451dba29383eb2e36421bd45aec28b121ca0b1
darlhet <76957966+darlhet@users.noreply.github.com>
2021-02-21 23:54:49 +0100

    changed some parameters

--- main_loops.sh
+++ main_loops.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/bash
-
 time="1"
 num="129"
-nsteps=("10" "20" "50" "100" "1000")
-integrator=("0" "1")
+nsteps=("10" "20" "50" "100" "200" "1000")
+integrator=("0" "1" "2")
 potential="3"
-
+#size of wall (N+1)/2 -- (N+5)/2  height  10
 for nstep in "${nsteps[@]}"
 do
   for int in "${integrator[@]}"
@@ -14,7 +13,7 @@ do
     do
       for n in "${num[@]}"
       do
-        ./main.exe $n ${time} $nstep $int $pot 12 6 1
+        ./main.exe $n ${time} $nstep $int $pot 30 6 1
       done
     done
   done

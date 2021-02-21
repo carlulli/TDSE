63e2ef275385a6803c59cd06ebcd929c3b8cf8bb
Adronis <astistroth@gmail.com>
2021-02-22 00:55:10 +0100

    second run same params

--- main.c
+++ main.c
@@ -44,7 +44,12 @@ set_kinetic_params(mass);
 set_potential(pot_choice);
 print_hamiltonian_info();
 
-printf("The program will calculate the TDSE of an initial wave packet with the following parameters input by the user:\n\nLattice size N = %d\nTotal run time t = %f\nNumber of steps nsteps = %d\nChoice of integrator (Euler [1] UCN [2] Strang-Splitting[3]) = %d\nChoice of Potential = %d\nParameters of the gaussian wavepacket mu = %f\t dx = %f\t dp = %f\n",N,ttime,ntimesteps,integrator_choice,pot_choice,mu,dx,dp);
+printf(
+  "The program will calculate the TDSE of an initial wave packet with the following parameters input by the user:\n"
+  "\nLattice size N = %d\nTotal run time t = %f\nNumber of steps nsteps = %d\nChoice of integrator (Euler [1] UCN [2] Strang-Splitting[3]) = %d\n"
+  "Choice of Potential = %d\nParameters of the gaussian wavepacket mu = %f\t dx = %f\t dp = %f\n",
+  N,ttime,ntimesteps,integrator_choice,pot_choice,mu,dx,dp
+);
 
 /* generates the gaussian wave packet */
 double complex psi[N];
@@ -63,11 +68,13 @@ snprintf(
   "data/gauss_wf_wp_%s_%s_%s_%s_%s_%s_%s_%s.txt", argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], argv[7], argv[8]);
 
 fp = fopen(filename, "w");
-fprintf(fp, "\nREAL(psi[n])\tIMAG(psi[n])\ttau\taverx\tdeltax\taverp\tdeltap\n");
+fprintf(fp, "\ntau\tREAL(psi[n])\tIMAG(psi[n])\taverx\tdeltax\taverp\tdeltap\tavg_state_energy\tnorm(psi)\n");
 for (int q = 0; q < ntimesteps; q++) {
   integrator(psi,tau*q,integrator_choice);
   for (int i = 0; i < N; i++) {
-  fprintf(fp,"%.12e\t%.12e\t%.12e\t%.12e\t%.12e\t%.12e\t%.12e\n",creal(psi[i]),cimag(psi[i]),tau*q,get_avgx(psi),get_deltax(psi),get_avgp(psi),get_deltap(psi));
+  fprintf(
+    fp,"%.12e\t%.12e\t%.12e\t%.12e\t%.12e\t%.12e\t%.12e\t%.12e\t%.12e\n",
+    tau*q,creal(psi[i]),cimag(psi[i]),get_avgx(psi),get_deltax(psi),get_avgp(psi),get_deltap(psi), average_state_energy(psi),norm(psi, N));
   }
 }
 fclose(fp);

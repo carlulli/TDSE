230f12ef1d3c7b24e2b7dc644a9971a4ba99a345
Adronis <astistroth@gmail.com>
2021-03-10 21:37:35 +0100

    jupyter notebook plotting script with pandas

--- main_loops.sh
+++ main_loops.sh
@@ -18,3 +18,24 @@ do
     done
   done
 done
+
+#!/usr/bin/bash
+time="1"
+num="129"
+nsteps=("1000" "2000" "5000" "10000")
+integrator=("0")
+potential="3"
+#size of wall (N+1)/2 -- (N+5)/2  height  10
+for nstep in "${nsteps[@]}"
+do
+  for int in "${integrator[@]}"
+  do
+    for pot in "${potential[@]}"
+    do
+      for n in "${num[@]}"
+      do
+        ./main.exe $n ${time} $nstep $int $pot 30 6 1
+      done
+    done
+  done
+done

fcbd38f80f53315dec9f49d6c3933838e9972d97
Adronis <astistroth@gmail.com>
2021-03-10 11:03:54 +0100

    data for SSM and UCM with wall height 6

--- main_loops.sh
+++ main_loops.sh
@@ -2,7 +2,7 @@
 time="1"
 num="129"
 nsteps=("10" "20" "50" "100" "200" "1000")
-integrator=("0" "1" "2")
+integrator=("1" "2")
 potential="3"
 #size of wall (N+1)/2 -- (N+5)/2  height  10
 for nstep in "${nsteps[@]}"
